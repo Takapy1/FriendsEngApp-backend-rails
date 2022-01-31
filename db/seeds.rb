3a234e0b9ef09bfe1306f9de303695641b841c38
Takapy1 <takahirog1109mai@gmail.com>
2022-01-31 10:17:08 +0900

    create season and episode by seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+(1..10).each do |_s|
+  Season.create
+end
+
+transcripts_path= Rails.root.to_s + "/public/transcripts"
+(1..10).each do |season_id|
+  html_file_names = Dir.open(transcripts_path+ "/html/season#{season_id}").children
+  num_episodes = html_file_names.size
+  num_episodes.times.map{ Episode.create(season_id: season_id) }
+end
